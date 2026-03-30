2033c1edce6914a186a731d46820f4667ea8456c
bhagatankit05 <bhagatankit82@gmail.com>
2026-03-30 15:47:41 +0530

    feat: add session management with express-session and connect-mongo
    - Updated package.json to include dependencies for session management: express-session, connect-mongo, bcrypt, and @google/genai.
    - Implemented session middleware in server.ts to manage user sessions.
    - Configured CORS to allow requests from specified origins.
    - Added AuthRouter for handling authentication routes.
    - Extended express-session to include user session data types.

--- server/server.ts
+++ server/server.ts
@@ -2,20 +2,51 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import AuthRouter from "./routes/AuthRoutes.js";
 
+
+ declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string;
+    }
+ }
 
 await connectDB();
 const app = express();
 
 // Middleware
-app.use(cors())
+app.use(cors(
+    {origin:['http://localhost:5173', 'http://localhost:3000/'],
+    credentials:true}
+))
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie:{maxAge: 1000 * 60 * 60 * 24 * 7}, // 7 day
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+}));
+
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+// Routes
+app.use('/api/auth',AuthRouter)
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
